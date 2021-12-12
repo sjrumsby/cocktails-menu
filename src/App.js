9837015e1ae2a64e7863f27d009c1cb1ead9f1a0
Sean Rumsby <sean@myhubly.com>
2021-12-12 15:33:40 -0800

    Added ABV tags for beers and wines

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { Percent } from 'react-bootstrap-icons';
 import Card from 'react-bootstrap/Card'
 import Container from 'react-bootstrap/Container'
 import Image from 'react-bootstrap/Image'
@@ -8,7 +9,7 @@ function App() {
   return (
     <div className="App">
       <Image fluid src="/banner.png" style={{ height: 'auto', maxWidth: '100%' }} />
-      <Container style={{ maxWidth: "700px" }}>
+      <Container style={{ marginTop: "1em", maxWidth: "700px" }}>
         <h1>Cocktails</h1>
         <Container className="m-1">
           { data.cocktails.map((cocktail) => {
@@ -26,6 +27,10 @@ function App() {
                   <Card.Title>{wine.name}</Card.Title>
                   <Card.Text>{wine.description}</Card.Text>
                 </Card.Body>
+                <Card.Body style={{ paddingTop: ".25em"}}>
+                  <Percent style={{ marginBottom: "4px" }}/>
+                  <span style={{ marginLeft: ".25em", marginTop: "1px" }}>{wine.tags.find((tag) => tag.type === "abv").value}</span>
+                </Card.Body>
               </Card>
             )
           })}
@@ -35,9 +40,13 @@ function App() {
           { data.beers.map((beer) => {
             return (
               <Card key={beer.name} style={{ border: "0", paddingBottom: "1em" }}>
-                <Card.Body>
+                <Card.Body style={{ paddingBottom: ".25em"}}>
                   <Card.Title>{beer.name}</Card.Title>
                   <Card.Text>{beer.description}</Card.Text>
+                </Card.Body>
+                <Card.Body style={{ paddingTop: ".25em"}}>
+                  <Percent style={{ marginBottom: "4px" }}/>
+                  <span style={{ marginLeft: ".25em", marginTop: "1px" }}>{beer.tags.find((tag) => tag.type === "abv").value}</span>
                 </Card.Body>
               </Card>
             )
